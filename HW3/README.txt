ad130f5e16d37e918db11a470d70071c5b1fbf3d
Zhongze Tang <i@tbis.me>
2018-02-23 18:13:07 -0500

    finish HW3

---
+++ HW3/README.txt
@@ -0,0 +1,18 @@
+
+Using Python 3.6.3.
+
+You still have to install these libraries for Python3.
+ - numpy
+ - matplotlib
+ - csv
+ - arrow
+
+
+Enter the root folder, type in
+
+python main.py
+
+in terminal or just click "Run" in IDE.
+
+
+Datasets are in the datasets forlders. Data should be organized like the csv files in this folder. (The "timestamp" and the "close" are the two most important columns.) About 30 records in a file is preferred.
--- HW3/main.py
+++ HW3/main.py
@@ -1,13 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from bayesian_regressor import BayesianRegressor
-from polynomial import PolynomialFeatures
 import csv
 import arrow
 
-alpha = 0.005
+alpha = 5e-3
 beta = 11.1
 degree = 9
+data_folder = './datasets'
+data_files = ['GOOG_0.csv', 'GOOG_1.csv', 'GOOG_2.csv', 'GOOG_2.csv', 'GOOG_3.csv'
+    , 'GOOG_4.csv', 'GOOG_5.csv', 'GOOG_6.csv', 'GOOG_7.csv', 'GOOG_8.csv', 'GOOG_9.csv'
+    , 'GOOG_10.csv', 'GOOG_rest.csv']
+
 
 def read_in_csv(file_name):
     x_raw = []
@@ -17,91 +20,82 @@ def read_in_csv(file_name):
     with open(file_name) as cf:
         reader = csv.DictReader(cf)
         row = reader.__next__()
+        # get timestamp and the price of close today
         x_today.append(arrow.get(row['timestamp']).replace(tzinfo='US/Pacific').timestamp)
         y_today.append(float(row['close']))
         for row in reader:
+            # get timestamp and the price of close every day before
             timestamp = arrow.get(row['timestamp']).replace(tzinfo='US/Pacific').timestamp
             x_raw.append(timestamp)
             y_raw.append(float(row['close']))
-        time_last = arrow.get(row['timestamp']).replace(tzinfo='US/Pacific').timestamp
-        x_raw = [int((i-time_last)/86400) for i in x_raw]
-        x_today = [int((x_today[0]-time_last)/86400)]
+        #get the earliest timestamp
+        time_earliest = arrow.get(row['timestamp']).replace(tzinfo='US/Pacific').timestamp
+        #calculate the number of days from the earliest day
+        x_raw = [int((i-time_earliest)/86400) for i in x_raw]
+        x_today = [int((x_today[0]-time_earliest)/86400)]
     return np.asarray(x_raw), np.asarray(y_raw), np.asarray(x_today), np.asarray(y_today)
 
-x_all, y_all, x_t, y_t = read_in_csv('./datasets/GOOG.csv')
-x_train = x_all
-y_train = y_all
-x_test = x_all
-y_test = y_all
 
 def phi(x):
-    phi = [x**i for i in range(degree + 1)]
+    phi = [[x**i] for i in range(degree + 1)]
     return np.asarray(phi)
 
-#formula (1.72)
-def S():
-    S_inv = alpha*np.identity(degree+1) + beta*np.sum([phi(x).dot(phi(x).T) for x in x_train], axis=0)
-    S = np.linalg.inv(S_inv)
-    return S
 
-#formula (1.70)
+# formula (1.70), mean
 def mx(x):
-    return beta*(phi(x).T).dot(S()).dot(np.sum([phi(xt)*t for xt, t in zip(x_train, y_train)], axis=0))
+    return beta*(phi(x).T).dot(S).dot(np.sum([t*phi(xt) for xt, t in zip(x_train, y_train)], axis=0))[0][0]
 
-#formula (1.71)
+
+# formula (1.71), variance
 def s2x(x):
-    return 1/beta+ (phi(x).T).dot(S()).dot(phi(x))
+    return (1/beta + (phi(x).T).dot(S.dot(phi(x))))[0][0]
 
 # formula (1.69)
-def Gaussian(xx, mx, s2x):
-    # miu = mx[0]
-    # sigma2 = s2x[0][0]
-    print(2*np.pi*s2x)
-    term1 = 1/np.sqrt(2*np.pi*s2x)
-    term2 = -1/(2*s2x)*(xx-mx)**2
-    return term1*np.exp(term2)
+# def Gaussian(xx, mx, s2x):
+for data_file in data_files:
+    print(data_file)
+    data_name = data_folder + '/' + data_file
+    x_all, y_all, x_t, y_t = read_in_csv(data_name)
+    '''
+    # x_train = x_all[int(len(x_all)/3):]
+    # y_train = y_all[int(len(y_all)/3):]
+    # x_test = x_all[:int(len(x_all)/3)]
+    # y_test = y_all[:int(len(y_all)/3)]
+    '''
+    # x_train = x_all[::-1]
+    # y_train = y_all[::-1]
+    N = len(x_all)
+    x_train = np.arange(0, 1.0, 1.0 / N)
+    y_train = y_all[::-1]
+    x_test  = np.arange(0, 1.0 + 1.0 / N, 1.0 / N)
 
-a=s2x(x_train)
-b=mx(x_train)
-print(a)
-Gaussian(x_train,b,a)
+    # print(x_train)
+    # print(y_train)
 
-# x_train = x_all[int(len(x_all)/3):]
-# y_train = y_all[int(len(y_all)/3):]
-# x_test = x_all[:int(len(x_all)/3)]
-# y_test = y_all[:int(len(y_all)/3)]
+    # formula (1.72)
+    S_inv = alpha * np.identity(degree + 1) + beta * np.sum([phi(x).dot(phi(x).T) for x in x_train], axis=0)
+    S = np.linalg.inv(S_inv)
 
-feature = PolynomialFeatures(degree)
-X_train = feature.transform(x_train)
-X_test = feature.transform(x_test)
+    absolute_mean_err = 0
+    ave_relative_err = 0
 
-model = BayesianRegressor(alpha=0.005, beta=11.1)
-model.fit(X_train, y_train)
+    for i in range(0, N-1):
+        absolute_mean_err = absolute_mean_err + abs(y_train[i] - mx(x_train[i]))
+        ave_relative_err = ave_relative_err + abs(y_train[i] - mx(x_train[i]))/y_train[i]
 
-y, y_err = model.predict(X_test, return_std=True)
-# print('x_t',x_t)
-# print('y_t',y_t)
-# print('y',y)
-# print('y_err',y_err)
+    absolute_mean_err = absolute_mean_err / N
+    ave_relative_err = ave_relative_err / N
 
-# plt.scatter(x_train, y_train, facecolor="none", edgecolor="b", s=5, label="training data")
-plt.plot(x_train, y_train, c="b")
-# plt.plot(x_test, y_test, c="g", label="$\sin(2\pi x)$")
+    # plt.plot(x_test, [mx(x) for x in x_test], color='0')
+    # for x, t in zip(x_train, y_train):
+    #     plt.scatter(x, t, color='b')
 
+    # print(x_test[-2])
+    print("The prediction of N+1 time is", mx(x_test[-2]),"±", 3*s2x(x_test[-2]))
+    print("The real value is", y_t)
 
-plt.plot(x_test, y, c="r", label="mean")
-# plt.scatter(x_test,y,facecolor = "none", edgecolor="b",s=50)
+    print("The absolute mean error is", absolute_mean_err)
+    print("The average relative error is", ave_relative_err)
 
-plt.fill_between(x_test, y - y_err, y + y_err, color="pink", label="std.", alpha=0.5)
-# plt.xlim(0,1500)
-# plt.ylim(300,1400)
-
-
-#sin函数的范围
-# plt.xlim(-0.1, 1.1)
-# plt.ylim(-1.5, 1.5)
-
-# 图例喝M = 9
-# plt.annotate("M=9", xy=(0.8, 1))
-# plt.legend(bbox_to_anchor=(1.05, 1.), loc=2, borderaxespad=0.)
-plt.show()
+    print()
+    # plt.show()
